c49dffbb103215e611ae8e84413572515929f559
Michael <wood.public@gmail.com>
2013-06-20 12:30:13 -0400

    Fixes to finding function calls

--- Phaeax_functions.py
+++ Phaeax_functions.py
@@ -37,21 +37,30 @@ def find_function_calls(filename):
         
         strings = re.findall(r'[def ]*[a-zA-Z_]+[\w.]*\(', f.read())
 
+        out_strings = []
+        
         for ii in range(len(strings)):
                 match = re.search(r'^def[ ]*',strings[ii])
 
                 if not match:
-                        m = re.search(r'([\w]*).+([\w]*)',strings[ii])
+                        m = re.search(r'([\w]*)(\.)([\w]*)',strings[ii])
+
+                        if m:
+                                out_strings.append('Call to: '+m.groups()[2])
+                                out_strings.append('   From: '+m.groups()[0])
+                                continue
+
+                        n = re.search(r'( )*([\w]*)',strings[ii])
+
+                        if n:
+                                out_strings.append('Call to: '+n.groups()[1])
+                                out_strings.append('   From: '+filename)
+                                continue
+                        else:
+                                print 'incorrect call format: '+strings[ii]
                         
 
-
-def num_groups(regex):
-    return re.compile(regex).groups
-
-
-
-
-
+        return out_strings
 
 
 
