6144bdc573a798e9c7b9737890afcf3770c83af4
Michael Wood <wood.public@gmail.com>
2013-06-23 09:32:03 -0400

    New functions

--- Phaeax.py
+++ Phaeax.py
@@ -11,10 +11,12 @@ import Phaeax_functions
 def main():
     print Phaeax_functions.find_imports('Test_def_demo.py')
     print Phaeax_functions.find_defs('Test_def_demo.py')
-    out = Phaeax_functions.find_function_calls('Test_def_demo.py')
+    out = Phaeax_functions.find_function_paths('Test_def_demo.py')
 
     for ele in out:
         print ele
+
+    print Phaeax_functions.find_full_function_calls('Test_def_demo.py')
 
 
 main()
--- Phaeax_functions.py
+++ Phaeax_functions.py
@@ -30,7 +30,7 @@ def find_defs(filename):
         return strings
 
 
-def find_function_calls(filename):
+def find_function_paths(filename):
 	
 	f = open(filename, 'r')
         
@@ -58,6 +58,35 @@ def find_function_calls(filename):
                                         else:
                                                 out_strings.append('   From: '+m.groups()[0]+'.py')
                                                 
+                                continue
+ 
+                        n = re.search(r'( )*([\w]*)',strings[ii])
+ 
+                        if n:
+                                out_strings.append('Call to: '+n.groups()[1])
+                                out_strings.append('   From: '+filename)
+                                continue
+                        else:
+                                print 'incorrect call format: '+strings[ii]                        
+ 
+        return out_strings
+
+
+def find_full_function_calls(filename):
+	
+	f = open(filename, 'r')
+        
+        strings = re.findall(r'[def ]*[a-zA-Z_]+[\w.]*\(', f.read())
+
+        out_strings = []
+        
+        for ii in range(len(strings)):
+                match = re.search(r'^def[ ]*',strings[ii])
+                
+                if not match:
+                        m = re.search(r'([\w.]*)',strings[ii])
+                        if m:
+                                out_strings.append('Call to: '+m.groups()[0]+'.py')
                                 continue
  
                         n = re.search(r'( )*([\w]*)',strings[ii])
