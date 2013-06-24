e0ef0945b59dc22a10f57f093dc4a6b616b4e3ae
Michael <wood.public@gmail.com>
2013-06-24 10:01:21 -0400

    New call dictionary, added pretty printing

--- Phaeax.py
+++ Phaeax.py
@@ -9,14 +9,12 @@ June 19, 2013
 import Phaeax_functions
 
 def main():
-    print Phaeax_functions.find_imports('Test_def_demo.py')
-    print Phaeax_functions.find_defs('Test_def_demo.py')
-    out = Phaeax_functions.find_function_paths('Test_def_demo.py')
-
-    for ele in out:
-        print ele
-
-    print Phaeax_functions.find_full_function_calls('Test_def_demo.py')
+    imports = Phaeax_functions.find_imports('Test_def_demo.py')
+    defs =  Phaeax_functions.find_defs('Test_def_demo.py')
+    paths = Phaeax_functions.find_function_paths('Test_def_demo.py')
+    call_dict = Phaeax_functions.find_full_function_calls('Test_def_demo.py')
 
 
+    Phaeax_functions.print_file_analysis('Test_def_demo.py', imports, defs, paths, call_dict)
+    
 main()
--- Phaeax_functions.py
+++ Phaeax_functions.py
@@ -78,24 +78,41 @@ def find_full_function_calls(filename):
         
         strings = re.findall(r'[def ]*[a-zA-Z_]+[\w.]*\(', f.read())
 
-        out_strings = []
+        function_calls_dict = {}
         
         for ii in range(len(strings)):
                 match = re.search(r'^def[ ]*',strings[ii])
                 
                 if not match:
-                        m = re.search(r'([\w.]*)',strings[ii])
+                        m = re.search(r'( )*([\w.]*)',strings[ii])
                         if m:
-                                out_strings.append('Call to: '+m.groups()[0]+'.py')
-                                continue
+                                function_calls_dict[m.groups()[1]] = function_calls_dict.get(m.groups()[1],0) + 1
+
  
-                        n = re.search(r'( )*([\w]*)',strings[ii])
- 
-                        if n:
-                                out_strings.append('Call to: '+n.groups()[1])
-                                out_strings.append('   From: '+filename)
-                                continue
-                        else:
-                                print 'incorrect call format: '+strings[ii]                        
- 
-        return out_strings
+        return function_calls_dict
+
+
+def print_file_analysis(filename, imports, defs, paths, call_dict):
+
+        print 'Analysis of '+filename+':'
+        print ' '
+        print ' '
+
+        print 'Import statements:'
+        for im in imports:
+                print im
+        print ' '
+
+        print 'Defined functions:'
+        for d in defs:
+                print d
+        print ' '
+
+        print 'List of called fuctions: '
+        key_order = sorted(call_dict,key=call_dict.get)
+        key_order.reverse()
+
+        for k in key_order:
+                print str(k)+' '+str(call_dict[k])
+        
+        
