663955cbe8e0fe4bfd37483d99da32ce08295676
Michael Wood <wood.public@gmail.com>
2013-07-08 07:00:06 -0400

    Planning

--- Phaeax.py
+++ Phaeax.py
@@ -6,6 +6,16 @@ Michael Wood
 June 19, 2013
 '''
 
+'''Current status:
+
+1. Single file version fully working as a demo
+2. Still need to re-impliment the print-out and error checking
+3. Next up is allowing for multiple input files where the outputs are stored
+   as an array of dictionaries (or a dict where key = filename, val = dict)
+4. After that, we will need functions to find connections between each file
+'''
+
+
 import Phaeax_functions
 
 def main(filename):
